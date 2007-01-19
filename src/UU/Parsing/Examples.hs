a62f6276bf331eeab15699eeff16b8f277dd2ea5
doaitse <doaitse@30b93d28-2606-0410-b7f1-95e0edb40354>
2007-01-19 16:16:41 +0000

    Minor change

--- src/UU/Parsing/Examples.hs
+++ src/UU/Parsing/Examples.hs
@@ -1,22 +1,19 @@
 module Examples where
 
 import UU.Parsing
+import UU.Parsing.CharParser
 
-instance Symbol Char where
-  symBefore = pred
-
-a, b, c :: P Char
 a = pSym 'a'
 b = pSym 'b'
 c = pSym 'c'
 
-type P = AnaParser [Char] Pair Char (Maybe Char) 
-
-test p inp = do result <- parseIOMessage show p inp
+test p inp = do result <- parseIO p inp
                 putStrLn (show result)
 
-t1 = test a "a"
-t2 = test b "a"
+ta = test a "a"
+tb = test b "a"
+tc = test c "abc"
+
 t3 = test (pToks "xyz" ) "xy"
 t4 = test (pToks "xyz" ) "xz"
 
@@ -28,4 +25,4 @@ if_as_keyword = ((("This is the keyword: ")    ++) <$> pToks "if")
 t5 = test if_as_ident   "if"
 t6 = test if_as_keyword "if"
 
-t7 = test (if_as_ident <* pCost 1 <|> if_as_keyword ) "if"
+t7 = test (if_as_ident <* pCost 2 <|> if_as_keyword <* pCost 2) "if"
